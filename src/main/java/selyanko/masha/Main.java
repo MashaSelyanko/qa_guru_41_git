c176003c820feeb7ca411d7927f2d5e447a33ee0
Mariya <158087859+MashaSelyanko@users.noreply.github.com>
2026-03-25 11:44:21 +0400

    Update Main.java

--- src/main/java/selyanko/masha/Main.java
+++ src/main/java/selyanko/masha/Main.java
@@ -1,13 +1,15 @@
 package selyanko.masha;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+
+
+
 public class Main {
     public static void main(String[] args) {
 
-        private static String msg = "Hello World!!!!";
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        private static String msg = "Hello!";
+       
         System.out.println(msg);
 
 
