58eeeeda85770a116707c4ec3913112c05f48f8e
Masha Selyanko <Masha.Selyanko@gmail.com>
2026-03-30 12:00:43 +0400

    add negative test

--- src/test/java/CheckFormNegative.java
+++ src/test/java/CheckFormNegative.java
@@ -1,15 +1,14 @@
-import com.codeborne.selenide.ClickOptions;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.files.DownloadActions.click;
 
 
-public class CheckFormNegative extends TestBase {
+class checkFormMinimum extends TestBase {
 
     @Test
-    void CheckForm() {
+    void CheckFormMinimum() {
         open("https://qa-guru.github.io/one-page-form/automation-practice-form.html");
 
         executeJavaScript("$('#fixedban').remove()");
@@ -32,6 +31,21 @@ public class CheckFormNegative extends TestBase {
         $("[id=closeModal]").click();
 
     }
+
+
+    @Test
+void CheckFormNegative() {
+    open("https://qa-guru.github.io/one-page-form/automation-practice-form.html");
+
+    executeJavaScript("$('#fixedban').remove()");
+    executeJavaScript("$('footer').remove()"); //скрываем рекламу
+
+    $("#firstName").setValue("0");
+    $("#submit").click();
+
+//проверка отображения ошибки заполнения
+   $("#formError").shouldHave(text("Please fill required fields and enter a valid 10-digit mobile number."));
+   $("#firstName").shouldBe(visible);
 }
 
-
+}
