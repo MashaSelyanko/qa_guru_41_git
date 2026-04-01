730c00a7393e5fa58b0309c1083171e4202a97f9
Masha Selyanko <Masha.Selyanko@gmail.com>
2026-04-01 23:20:39 +0400

    add arithmetic operations

--- src/main/java/selyanko/masha/Main.java
+++ src/main/java/selyanko/masha/Main.java
@@ -3,15 +3,33 @@ package selyanko.masha;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public static void main(String[] args) {
+    int a = 100;
+    int b = 200;
+    double c = 2.5;
+    double d = 2.55;
+    byte f = 127;
+    byte g = 99;
+    byte result = (byte) (f*g);
+
+    System.out.println("a+b= " + (a+b));
+    System.out.println("b-a= " + (b-a));
+    //System.out.println("b/a= " + (b/a));
+    System.out.println("a*b= " + (a*b));
+
+    System.out.println("a/c= " + (a/c));
+    System.out.println("a/d= " + (a%d));
+
+    if (a == b) {
+        System.out.println("покупаем");
+    } else if (a>c) {
+        System.out.println("продаем");
+    } else {
+        System.out.println("держим");
     }
+
+    System.out.println("f*g= " + (f*g));
+
+}
 }
