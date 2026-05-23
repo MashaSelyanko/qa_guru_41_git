591921d87cc9bef035367317af3f065ef0c4f738
masha <selyankoMariya@gmail.com>
2026-05-23 14:57:43 +0400

    refactor: remove unused imports

--- src/test/java/fileParsingTests/FileParsingTests.java
+++ src/test/java/fileParsingTests/FileParsingTests.java
@@ -5,14 +5,11 @@ import com.codeborne.xlstest.XLS;
 import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
 
 public class FileParsingTests {
