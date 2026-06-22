a92253184ab5acc7aadd25d55b9aa8e385ca660c
masha <selyankoMariya@gmail.com>
2026-06-22 22:06:48 +0400

    добавлены проверки на пустой файл в архиве

--- src/test/java/fileParsingTests/FileParsingTests.java
+++ src/test/java/fileParsingTests/FileParsingTests.java
@@ -5,6 +5,7 @@ import com.codeborne.xlstest.XLS;
 import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
@@ -19,56 +20,68 @@ public class FileParsingTests {
     //читаем содержимое zip- архива
     @Test
     void pdfFileParsingTest() throws Exception {
-        try (InputStream is = cl.getResourceAsStream("TestsFile.zip");
-             ZipInputStream zis = new ZipInputStream(is)) {
-              ZipEntry entry;
+        boolean found = false;
+        try (
+                InputStream is = cl.getResourceAsStream("TestsFile.zip");
+                ZipInputStream zis = new ZipInputStream(is)) {
+            ZipEntry entry;
 
             while ((entry = zis.getNextEntry()) != null) {
+                found = true;
                 if (entry.getName().endsWith("pdf")) {
                     PDF pdf = new PDF(zis);
                     Assertions.assertTrue(pdf.text.contains("Lorem ipsum dolor sit amet"));
                 }
             }
         }
+        Assertions.assertTrue(found, "PDF файла нет в архиве");
     }
+
 
     @Test
     void xlsxFileParsingTest() throws Exception {
+        boolean found = false;
         try (InputStream is = cl.getResourceAsStream("TestsFile.zip");
              ZipInputStream zis = new ZipInputStream(is)) {
             ZipEntry entry;
 
             while ((entry = zis.getNextEntry()) != null) {
                 if (entry.getName().endsWith("xls")) {
+                    found = true;
                     XLS xls = new XLS(zis);
                     String actualValue = xls.excel.getSheetAt(0)
                             .getRow(3)
                             .getCell(2).getStringCellValue();
-                    Assertions.assertTrue(actualValue.contains("Jane"));
+                    Assertions.assertTrue(actualValue.contains("Jane"),
+                            "Не нашел значение"
+                    );
                 }
             }
         }
     }
 
-                @Test
-                void csvFileParsingTest () throws Exception {
-                    try (InputStream is = cl.getResourceAsStream("TestsFile.zip");
-                         ZipInputStream zis = new ZipInputStream(is)) {
-                        ZipEntry entry;
+    @Test
+    void csvFileParsingTest() throws Exception {
+        boolean found = false;
+        try (InputStream is = cl.getResourceAsStream("TestsFile.zip");
+             ZipInputStream zis = new ZipInputStream(is)) {
+            ZipEntry entry;
 
-                        while ((entry = zis.getNextEntry()) != null) {
-                            if (entry.getName().endsWith("csv")) {
-                                CSVReader csvReader = new CSVReader(new InputStreamReader(zis));
-                                List<String[]> data = csvReader.readAll();
-                                Assertions.assertEquals(11, data.size());
-                                Assertions.assertArrayEquals(
-                                        new String[]
-                                                {"id", "first_name", "last_name", "email", "age", "city", "salary", "joined"},
-                                        data.get(0)
-                                );
-                            }
-                        }
-                    }
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.getName().endsWith("csv")) {
+                    found = true;
+                    CSVReader csvReader = new CSVReader(new InputStreamReader(zis));
+                    List<String[]> data = csvReader.readAll();
+                    Assertions.assertEquals(11, data.size());
+                    Assertions.assertArrayEquals(
+                            new String[]
+                                    {"id", "first_name", "last_name", "email", "age", "city", "salary", "joined"},
+                            data.get(0)
+                    );
                 }
             }
+        }
+        Assertions.assertTrue(found, "CSV файла нет в архиве");
+    }
+}
 
